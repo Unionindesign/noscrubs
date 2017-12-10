d63e3316d2812e3ad373d4a720599608d3a90aab
Unionindesign <stevemarshall89@yahoo.com>
2017-12-10 09:03:23 -0700

    Add files via upload

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,1 +1,58 @@
+// No Scrubs Quiz!
+
+//start with button on click
+$( '#start-button' ).on('click',function() {
+
+    
+    // displays html
+    $('#scrub-questions').removeClass('hideQuiz');
+
+    //removes the start-button
+        // $('#scrub-header').remove();
+
+
+    // if (scrub-questions.style.display === "none") {
+    //     scrub-questions.style.display = "block";
+    // } else {
+    //     scrub-questions.style.display = "none";
+    
+
+})
+
+
+
+    // function quizVisible() {
+
+    //hide html div, so appears on start function
+            // $("scrub-questions").toggle();
+            
+            // $( "div:hidden" ).show( "fast" );
+    
+            // $( "#scrub-questions:hidden" ).show( "fast" );
+            
+            // var showQuiz = document.getElementById("scrub-questions");
+  
+    
+    
+
+    //countdown begins for xxx seconds
+    
+
+
+
+        //IF  scrubAnswer = True , add to scrubTotal
+
+        //TODO: radio buttons talk to js, only one can be selected
+       
+        // else if false, add to noScrubTotal 
+
+        //timeout, or when all questions answered <- ??
+
+            //Calculate based on scrubTotal v no srub total
+            //Calculate based on div class .scrub-click (make a div class on just those answers, and then #12??/'scrub-clicks')
+                //if result in between 1-3, 3-5, 6-8,9-9..9 100% different results
+
+                // restart button
+
+                // home page button
 
